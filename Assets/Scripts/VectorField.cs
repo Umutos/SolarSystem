f79eda1165e4ed8575856ac8ffb6a111a4ed460b
Yann DECHAUX <y.dechaux@student.isartdigital.com>
2023-02-20 17:26:50 +0100

    skybox and basic architecture + vector point/fields gameobject and scripts

---
+++ Assets/Scripts/VectorField.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorField : MonoBehaviour
+{
+    public int range = 32;
+    public int step = 1;
+    private GameObject vectorPoint;
+    public string scalarField;
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/VectorPoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VectorPoint : MonoBehaviour
+{
+    [HideInInspector] public Vector3 position;
+    [HideInInspector] public Vector3 vectorCoordinnate;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
