72352c0832fb7bbf4641c40eb23e56bfcb5bc19b
Antoine MORDANT <a.mordant@student.isartdigital.com>
2023-03-22 19:11:34 +0100

    add fieldline

--- Assets/Scripts/FieldLine.cs
+++ Assets/Scripts/FieldLine.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class FieldLine : MonoBehaviour
 {
     public CelestialObject currentObject;
+    public SolarSystem solarSystem;
+    public int pointNumber = 10;
+
+    Vector3 acc;
+    Vector3 vel;
+    Vector3 pos;
+    List<Vector3> points;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +23,25 @@ public class FieldLine : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void CreateLine()
+    {
+        pos = currentObject.transform.position;
+        vel = currentObject.velocity;
+        acc = Vector3.zero;
+
+        for (int i = 0; i < pointNumber; i++)
+        {
+            foreach (CelestialObject curObject in solarSystem.planets)
+            {
+                if (curObject.GetInstanceID() == currentObject.GetInstanceID()) continue;
+
+                float distance = Vector3.Distance(curObject.transform.position, pos);
+                float force = 0.10f * (curObject.mass * currentObject.mass) / Mathf.Pow(distance, 2);
+                Vector3 direction = (pos - curObject.transform.position);
+                acc += direction * force;
+            }
+        }
     }
 }
