e2a85292bf03ca26e99e970e8f428578f07c799f
Umut Osmanoglu <u.osmanoglu@student.isartdigital.com>
2023-03-26 17:41:19 +0200

    Add Texture to new prefab

---
+++ Assets/Scripts/TextureDropdown.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextureDropdown : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private TMP_Dropdown textureDropdown;
+    [SerializeField] private List<Texture> textures;
+
+    private Texture currentSelectedTexture;
+
+    private void Start()
+    {
+        textureDropdown.onValueChanged.AddListener(delegate { ChangeTexture(textureDropdown); });
+        currentSelectedTexture = textures[0];
+    }
+
+    private void ChangeTexture(TMP_Dropdown dropdown)
+    {
+        currentSelectedTexture = textures[dropdown.value];
+        SetTexture(prefab, currentSelectedTexture);
+    }
+
+    private void SetTexture(GameObject obj, Texture texture)
+    {
+        Renderer renderer = obj.transform.GetChild(0).GetComponent<MeshRenderer>();
+        Material newMaterial = new Material(renderer.sharedMaterial);
+        newMaterial.mainTexture = texture;
+        renderer.material = newMaterial;
+    }
+}
